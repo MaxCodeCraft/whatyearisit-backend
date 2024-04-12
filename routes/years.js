6452d29fdcaa962fece16f777ca704ed6cfd7810
MaxCodeCraft <maxcodecraft06@gmail.com>
2024-04-12 09:56:27 +0200

    initial commit

---
+++ routes/years.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET year */
+router.get("/", (req, res) => {
+  res.json({ year: new Date().getFullYear() });
+});
+
+module.exports = router;
