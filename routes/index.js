fb074c379a604800afc27dc36d184f0fee463566
MaxCodeCraft <maxcodecraft06@gmail.com>
2024-04-12 10:39:59 +0200

    *

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-
 router.get("/year", (req, res) => {
   res.json({ year: new Date().getFullYear().toString() });
 });
